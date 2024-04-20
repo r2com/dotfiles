2fffbb52997b4c01efaa4d021baec97938eb7c20
r2com <r2com@proton.me>
2024-04-20 08:21:34 -0700

    Adding dotfiles for nvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.timeoutlen = 0
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.mouse = "a"
+vim.opt.splitright = true
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.textwidth = 80
+vim.opt.confirm = true
---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,55 @@
+return {
+	--{
+	--	"lewis6991/gitsigns.nvim",
+	--	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+	--	opts = {
+	--		signs = {
+	--			add = { text = "▎" },
+	--			change = { text = "▎" },
+	--			delete = { text = "" },
+	--			topdelete = { text = "" },
+	--			changedelete = { text = "▎" },
+	--			untracked = { text = "▎" },
+	--		},
+	--		current_line_blame = true,
+	--		current_line_blame_opts = {
+	--			virt_text = true,
+	--			virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+	--			virt_text_priority = 100,
+	--			delay = 1000,
+	--		},
+	--		_signs_staged_enable = true,
+	--		on_attach = function(buffer)
+	--			local gs = package.loaded.gitsigns
+	--			require("config.mappings").git_attach(gs, buffer)
+	--		end,
+	--	},
+	--},
+	{
+		"sindrets/diffview.nvim",
+		keys = {
+			{
+				"<leader>gd",
+				function()
+					local lib = require("diffview.lib")
+					local view = lib.get_current_view()
+					if view then
+						-- Current tabpage is a Diffview; close it
+						vim.cmd(":DiffviewClose")
+					else
+						-- No open Diffview exists: open a new one
+						vim.cmd(":DiffviewOpen")
+					end
+				end,
+				desc = "Diff View",
+			},
+		},
+		opts = {
+			view = {
+				merge_tool = {
+					disable_diagnostics = true,
+				},
+			},
+		},
+	},
+}
